9c0b495f4288b1591d9cfa4b6f436b2f14801890
Rodrigo Estevao <rodrigoestevao@yahoo.com>
2021-03-21 08:58:27 -0300

    Checkpoint matters

--- app/demo/apps/chat/consumers.py
+++ app/demo/apps/chat/consumers.py
@@ -2,5 +2,11 @@ from channels.generic.websocket import WebsocketConsumer
 
 
 class ChatConsumer(WebsocketConsumer):
-    def function(self):
-        pass
+    def connect(self):
+        return super().connect()
+
+    def disconnect(self, code):
+        return super().disconnect(code)
+
+    def receive(self, text_data, bytes_data):
+        return super().receive(text_data=text_data, bytes_data=bytes_data)
